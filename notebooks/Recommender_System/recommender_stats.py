0a8d2377c04e89f52170eadda5081732003f5666
Kai Chuen Tan <71690670+kaitheuser@users.noreply.github.com>
2022-03-09 04:36:05 -0800

    Added Mechanics Recommendation to RS

--- notebooks/Recommender_System/recommender_stats.py
+++ notebooks/Recommender_System/recommender_stats.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def recommender_stats_surprise(full_data_path, mechanics_data_path, df_games):
     """
@@ -15,7 +16,8 @@ def recommender_stats_surprise(full_data_path, mechanics_data_path, df_games):
     df_csv_clean = pd.read_csv(full_data_path)
 
     # Read mechanics
-    df_csv_mechanics = pd.read_csv(mechanics_data_path)
+    df_csv_mechanics = pd.read_csv(mechanics_data_path).set_index('BGGId')
+    
 
     return_df = pd.DataFrame(columns=df_csv_clean.columns)
     for game in df_games['ID']:
@@ -23,4 +25,17 @@ def recommender_stats_surprise(full_data_path, mechanics_data_path, df_games):
 
     return_df = return_df.loc[:, ~(return_df.columns.str.contains('^Unnamed') | return_df.columns.str.contains('^Cat'))]
 
-    return return_df[['GameWeight', 'MinPlayers', 'MaxPlayers', 'ComAgeRec', 'MfgPlaytime']]
+    #df_final_output, df = recommender_stats_surprise(clean_csv_path, mechanics_csv, df_games)
+
+    return_df["Mechanics"] = ''
+    count = 0
+    for bggid in return_df['BGGId'].tolist():
+        mechanics_words = ''
+        for mechanic in df_csv_mechanics.columns.tolist():
+            if df_csv_mechanics.loc[[bggid], [mechanic]].values[0, 0] == 1:
+                mechanics_words = mechanics_words + mechanic + ', '
+        mechanics_words = mechanics_words[:-2]
+        return_df.at[count, 'Mechanics'] = mechanics_words
+        count+=1
+
+    return return_df[['GameWeight', 'MinPlayers', 'MaxPlayers', 'ComAgeRec', 'MfgPlaytime', 'Mechanics']]
